91473e2e309fac7b5e490858064773fcf98d1bce
CrazyCyberKing <114866933+CrazyCyberKing@users.noreply.github.com>
2023-04-05 16:33:06 +0530

    Update __main__.py

--- Yone/__main__.py
+++ Yone/__main__.py
@@ -85,15 +85,7 @@ def get_readable_time(seconds: int) -> str:
 
 PM_START_TEXT = """ Hello *{}*, My name is *{}*! 
 A telegram group management bot. I'm here to help you to manage your groups.
-I have lots of handy features such as:
-‣ Warning system
-‣ Artificial intelligence
-‣ Flood control system
-‣ Note keeping system
-‣ Filters keeping system
-‣ Approvals and much more.
 
-So what are you waiting for?
 *Add me in your groups and give me full rights to make me function well.*
 """
 
@@ -240,8 +232,8 @@ def yone_about_callback(update: Update, context: CallbackContext):
             reply_markup=InlineKeyboardMarkup(
                 [
                    [
-                     InlineKeyboardButton(text="Support", url="t.me/Yone_Support"),
-                     InlineKeyboardButton(text="News", url="t.me/Yone_Updates"),
+                     InlineKeyboardButton(text="Support", url="t.me/Serena_Support"),
+                     InlineKeyboardButton(text="News", url="t.me/Serena_Support"),
                    ],
                    [
                     InlineKeyboardButton(text="Back", callback_data="yone_back")
